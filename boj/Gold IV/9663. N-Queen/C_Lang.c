e69138a8cd0cfa68da183d9771992d404e0c0da3
koreakk <ahaseo11@gmail.com>
2022-10-05 22:47:22 +0900

    2022-10-05

---
+++ boj/Gold IV/9663. N-Queen/C_Lang.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int board[15][15];
+
+bool check(int n, int x, int y)
+{
+	for (int i = 0; i < y; ++i) {
+		if (board[i][x]) {			
+			return false;
+		}
+	}
+	
+	for (int i = y, j = x; i >= 0 && j >= 0; --i, --j) {
+		if (board[i][j]) {
+			return false;
+		}
+	}
+	
+	for (int i = y, j = x; i >= 0 && j < n; --i, ++j) {
+		if (board[i][j]) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+int dfs(int n, int y)
+{
+	if (y >= n) {
+		return 1;
+	}
+	
+	int result = 0;
+	
+	for (int x = 0; x < n; ++x) {
+		if (check(n, x, y)) {
+			board[y][x] = 1;
+			result += dfs(n, y + 1);
+			board[y][x] = 0;
+		}
+	}
+	
+	return result;
+}
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	
+	printf("%d\n", dfs(n, 0));
+	return 0;
+}
