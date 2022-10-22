90c19c004a6a94708f73a75e44ebe1f4ea79bebf
koreakk <ahaseo11@gmail.com>
2022-10-23 00:30:21 +0900

    2022-10-22

---
+++ boj/Level 1-5/1193. 분수찾기/Cpp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int x;
+	cin >> x;
+
+	int i = 1, j = x;
+	for (; j > i; j -= i++) {}
+	
+	if (i % 2) {
+		cout << i + 1 - j << '/' << j << endl;
+	} else {
+		cout << j << '/' << i + 1 - j << endl;
+	}
+	return 0;
+}
