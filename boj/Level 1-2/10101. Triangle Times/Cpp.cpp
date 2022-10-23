e39bef09b45d59ce6d7f7ac1006fe137aa6a3421
koreakk <ahaseo11@gmail.com>
2022-10-23 18:26:28 +0900

    2022-10-23

---
+++ boj/Level 1-2/10101. Triangle Times/Cpp.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int a, b, c;
+	cin >> a >> b >> c;
+	
+	if (a == 60 && b == 60 && c == 60) {
+		cout << "Equilateral" << endl;
+	} else if (a + b + c == 180) {
+		if (a != b && b != c && a != c) {
+			cout << "Scalene" << endl;
+		} else {
+			cout << "Isosceles" << endl;
+		}
+	} else {
+		cout << "Error" << endl;
+	}
+	return 0;
+}
