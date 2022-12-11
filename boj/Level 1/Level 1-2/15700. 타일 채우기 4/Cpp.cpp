df340e420943dcfd114dbf7c4b107319e882b168
koreakk <ahaseo11@gmail.com>
2022-12-11 21:38:34 +0900

    2022-12-11

---
+++ boj/Level 1/Level 1-2/15700. 타일 채우기 4/Cpp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long N, M;
+    cin >> N >> M;
+
+    cout << N * M / 2 << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
