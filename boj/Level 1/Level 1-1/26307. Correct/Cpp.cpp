603ab204d36edc5edcb4331d9bcf9c55bbe124b4
koreakk <ahaseo11@gmail.com>
2022-12-14 08:25:12 +0900

    2022-12-14

---
+++ boj/Level 1/Level 1-1/26307. Correct/Cpp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int H, M;
+    cin >> H >> M;
+
+    cout << (H - 9) * 60 + M << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
--- boj/Level 3/Level 3-5/1562. 계단 수/Cpp.cpp
+++ boj/Level 3/Level 3-5/1562. 계단 수/Cpp.cpp
@@ -38,7 +38,6 @@ void solve() {
     cout << ret << '\n';
 }
 
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
