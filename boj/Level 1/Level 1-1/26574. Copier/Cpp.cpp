9e56b6aadc86cb8826c6f91ab0719d56d09d4350
koreakk <ahaseo11@gmail.com>
2022-12-19 20:22:54 +0900

    2022-12-19

---
+++ boj/Level 1/Level 1-1/26574. Copier/Cpp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+
+        cout << x << ' ' << x << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
