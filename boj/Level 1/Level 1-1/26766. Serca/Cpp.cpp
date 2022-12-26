cbb66fcf4919a663df93f6c1b1a2026203dfad3c
koreakk <ahaseo11@gmail.com>
2022-12-26 19:52:00 +0900

    2022-12-26

---
+++ boj/Level 1/Level 1-1/26766. Serca/Cpp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cout << " @@@   @@@ \n"
+             << "@   @ @   @\n"
+             << "@    @    @\n"
+             << "@         @\n"
+             << " @       @ \n"
+             << "  @     @  \n"
+             << "   @   @   \n"
+             << "    @ @    \n"
+             << "     @     \n";
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
