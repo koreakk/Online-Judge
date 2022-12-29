5e05d2224a8d37b128ae9056c13d6676d9cef606
koreakk <ahaseo11@gmail.com>
2022-12-29 21:39:45 +0900

    2022-12-29

---
+++ boj/Level 1/Level 1-2/6887. Squares/Cpp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    cout << "The largest square has side length ";
+    cout << (int)sqrt(n) << ".\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
