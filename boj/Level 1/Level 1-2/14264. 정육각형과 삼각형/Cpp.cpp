add9b8360a8c5318b7ff33b156441b1c6050d51e
koreakk <ahaseo11@gmail.com>
2023-01-07 23:57:37 +0900

    2023-01-07

---
+++ boj/Level 1/Level 1-2/14264. 정육각형과 삼각형/Cpp.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    cout << sqrt(3) * n * n / 4;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout << fixed;
+    cout.precision(10);
+    solve();
+    return 0;
+}
