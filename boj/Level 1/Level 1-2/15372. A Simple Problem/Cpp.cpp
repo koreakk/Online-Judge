4ee466a41fba3998d31bde26645e5cd293bcc053
koreakk <ahaseo11@gmail.com>
2023-01-12 17:25:19 +0900

    2023-01-12

---
+++ boj/Level 1/Level 1-2/15372. A Simple Problem/Cpp.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int t;
+    cin >> t;
+    while (t--) {
+        unsigned long long n;
+        cin >> n;
+        cout << n * n << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
