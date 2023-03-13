16a1bd2d6f8e37c920f27c6f4acf3fe30c502bd7
minseo Kang <98510846+koreakk@users.noreply.github.com>
2023-03-13 23:12:16 +0900

    2023-03-13

---
+++ boj/Level 1/Level 1-3/2501. 약수 구하기/Cpp.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0) { --k; }
+        if (k == 0) {
+            cout << i << '\n';
+            break;
+        }
+    }
+    if (k) { cout << "0\n"; }
+
+    return 0;
+}
