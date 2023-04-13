9ff062a5e03c98715e9dda7dcbb5274dbae1ca25
minseo Kang <98510846+koreakk@users.noreply.github.com>
2023-04-13 20:45:28 +0900

    2023-04-13

---
+++ boj/Level 1/Level 1-2/15610. Abbey Courtyard/Cpp.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(7);
+
+    long double a;
+    cin >> a;
+
+    cout << sqrtl(a) * 4.0 << '\n';
+    return 0;
+}
