f31f3d3106116cb95f777abe14f6e3a7d3dd982d
minseo Kang <98510846+koreakk@users.noreply.github.com>
2023-05-19 12:59:22 +0900

    2023-05-19

---
+++ boj/Level 1/Level 1-3/10569. 다면체/Cpp.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int v, w;
+        cin >> v >> w;
+        cout << w-v+2 << '\n';
+    }
+    return 0;
+}
