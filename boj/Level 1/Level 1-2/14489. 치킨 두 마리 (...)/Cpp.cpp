02a32695615188bc54c1adeddfbf863975d5d2e6
minseo Kang <98510846+koreakk@users.noreply.github.com>
2023-08-05 18:31:44 +0900

    2023-08-05

---
+++ boj/Level 1/Level 1-2/14489. 치킨 두 마리 (...)/Cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a + b < 2*c) {
+        cout << a + b << '\n';
+    }
+    else {
+        cout << a + b - 2*c << '\n';
+    }
+    return 0;
+}
