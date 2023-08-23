2715fd57dbfaa2b8fc8e3ec4c7c9cdb97bde95f6
minseo Kang <98510846+koreakk@users.noreply.github.com>
2023-08-23 23:39:57 +0900

    2023-08-23

---
+++ boj/Level 1/Level 1-2/6941. I Speak TXTMSG/Python.py
@@ -0,0 +1,24 @@
+l = {
+'CU'    :  'see you',
+':-)'   :  'I’m happy',
+':-('   :  'I’m unhappy',
+';-)'   :  'wink',
+':-P'   :  'stick out my tongue',
+'(~.~)' :  'sleepy',
+'TA'    :  'totally awesome',
+'CCC'   :  'Canadian Computing Competition',
+'CUZ'   :  'because',
+'TY'    :  'thank-you',
+'YW'    :  'you’re welcome',
+'TTYL'  :  'talk to you later'
+}
+
+while True:
+    x = input()
+    if x in l:
+        print(l[x])
+    else:
+        print(x)
+
+    if (x == 'TTYL'):
+        break
